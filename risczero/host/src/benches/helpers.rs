70a11a5fe2c6a610ccb1e720a608b250f4dbdc45
Ilias Garnier <igarnier@protonmail.com>
2023-09-07 11:47:58 +0200

    Printing succinct proof seal size

--- risczero/host/src/benches/helpers.rs
+++ risczero/host/src/benches/helpers.rs
@@ -1,6 +1,19 @@
 // TODO: is there a cleaner way to compute the proof size? Something directly
 // exposed in the risc0 API?
+// pub fn inner_receipt_size_bytes(proof: &risc0_zkvm::receipt::InnerReceipt) -> u32 {
+//     let bytes: Vec<u8> = bincode::serialize(proof).unwrap();
+//     bytes.len() as u32
+// }
+
 pub fn inner_receipt_size_bytes(proof: &risc0_zkvm::receipt::InnerReceipt) -> u32 {
+    match proof {
+        risc0_zkvm::receipt::InnerReceipt::Flat(_) => {}
+        risc0_zkvm::receipt::InnerReceipt::Succinct(succinct) => {
+            let size = succinct.seal.len();
+            println!("Seal length: u32 elements = {}", size)
+        }
+        risc0_zkvm::receipt::InnerReceipt::Fake => {}
+    }
     let bytes: Vec<u8> = bincode::serialize(proof).unwrap();
     bytes.len() as u32
 }
